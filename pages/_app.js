3a1c8e33de4a1116e474e8bd110f029a9408fd37
Micael <micaelsilveira31@gmail.com>
2022-01-11 19:54:31 -0300

    _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,23 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Link from "next/link";
-import Feed from "../src/Components/Feed";
-// export async function getServerSideProps() {
-//   const response = await fetch(API_ASTRONOMY_DAY);
-//   const data = await response.json();
-//   return {
-//     props: {
-//       data: data,
-//     },
-//   };
-// }
-const Home = ({ data_Astronomy_day }) => {
+import "../styles/globals.css";
+import Header from "../src/Components/Header";
+import { API_ASTRONOMY_DAY } from "../src/api";
+import React from "react";
+function MyApp({ Component, pageProps }) {
+  const [data_Astronomy_day, setAstronomy_day] = React.useState(null);
+  React.useEffect(() => {
+    async function request() {
+      const response = await fetch(API_ASTRONOMY_DAY);
+      const data = await response.json();
+      setAstronomy_day(data);
+    }
+    request();
+  }, []);
   return (
-    <div>
-      <Head>
-        <title>DESENVOLVIMENTO</title>
-      </Head>
-      <div>
-        <Link href="/infor-today/Astronomy-day">
-          <a className={styles.today}>
-            <h3>Today</h3>
-          </a>
-        </Link>
-      </div>
-      {data_Astronomy_day && (
-        <Feed
-          data={data_Astronomy_day}
-          today={true}
-          img={data_Astronomy_day.url}
-          descri={false}
-          dataPost={false}
-        />
-      )}
-    </div>
+    <>
+      <Header />
+      <Component data_Astronomy_day={data_Astronomy_day} {...pageProps} />
+    </>
   );
-};
-export default Home;
+}
+
+export default MyApp;
