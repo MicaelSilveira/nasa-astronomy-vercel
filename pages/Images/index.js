141f1dd151c06ebf037514bdbdd4cd9fbb3ee2e9
Micael <micaelsilveira31@gmail.com>
2022-01-25 22:42:36 -0300

    scrollTOP

--- pages/Images/index.js
+++ pages/Images/index.js
@@ -10,6 +10,9 @@ const ImageHome = () => {
   React.useEffect(() => {
     async function requestFetch() {
       await request(`${API_IMAGES_LIBRARY_PAGE}${pageStats}`);
+      window.scrollTo({
+        top: 0,
+      });
     }
     requestFetch();
   }, [pageStats]);
